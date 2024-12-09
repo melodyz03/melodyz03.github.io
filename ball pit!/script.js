82ba7aab6404f9ee8d943f5724a93adbe1c2efa4
melodyz03 <melodyz@sas.upenn.edu>
2024-12-09 12:02:36 -0500

    Update script.js

--- ball pit!/script.js
+++ ball pit!/script.js
@@ -193,8 +193,8 @@ handleClick(event) {
             }
 
             // Normal ball handling
-            ball.velocityY = -Math.random() * 25 - 15;
-            ball.velocityX += (Math.random() - 0.5) * 10;
+            ball.velocityY = -Math.random() * 40 - 30;
+            ball.velocityX += (Math.random() - 0.5) * 15;
             //The negative vertical velocity makes the ball jump up, while the slight random horizontal velocity adds a bit of unpredictability to the movement.
             this.convertToPastel(ball.color);
             break; // Stop after first ball
